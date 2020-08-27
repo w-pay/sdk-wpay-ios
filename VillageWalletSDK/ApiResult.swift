ecdc8dca26142f263385dae2a76b9a40dea26873
Kieran Simpson <kieran@redcrew.com.au>
2020-08-27 18:48:00 +1000

    Create custom error type for possible API errors.

--- VillageWalletSDK/ApiResult.swift
+++ VillageWalletSDK/ApiResult.swift
@@ -1,3 +1,18 @@
 import UIKit
 
-public typealias ApiResult<T> = (T?, HTTPURLResponse?) -> Void
+public enum ApiError: Error {
+	case jsonEncoding
+	case jsonDecoding
+	case httpError(reason: HttpErrorReason, response: HTTPURLResponse)
+
+	// catch all
+	case error(error: Error)
+
+	public enum HttpErrorReason: Int {
+		case invalidInput = 400
+		case unauthorised = 401
+		case processingError = 422
+		case serverError = 500
+	}
+}
+
